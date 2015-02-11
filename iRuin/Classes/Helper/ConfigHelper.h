69643bdfda9c81eab835130471ae5114133664a7
isaacselement <isaacselement@gmail.com>
2015-02-11 18:32:29 +0800

    new version

---
+++ iRuin/Classes/Helper/ConfigHelper.h
@@ -0,0 +1,13 @@
+//
+//  ConfigHelper.h
+//  iRuin
+//
+//  Created by suchavision on 2/11/15.
+//  Copyright (c) 2015 isaacs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ConfigHelper : NSObject
+
+@end
---
+++ iRuin/Classes/Helper/ConfigHelper.m
@@ -0,0 +1,13 @@
+//
+//  ConfigHelper.m
+//  iRuin
+//
+//  Created by suchavision on 2/11/15.
+//  Copyright (c) 2015 isaacs. All rights reserved.
+//
+
+#import "ConfigHelper.h"
+
+@implementation ConfigHelper
+
+@end
